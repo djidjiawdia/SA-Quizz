79b8da5e8e0a32eaa01079a98b11513b19d280c7
djidjiawdia <djidjiawdiacfc@gmail.com>
2020-03-19 16:24:28 +0100

    Front End: Updated

--- Pages/admin/creer_question.php
+++ Pages/admin/creer_question.php
@@ -1,24 +1,53 @@
 <div class="admin-card creerQ">
     <h1>Paramétrer votre question</h1>
-    <form class="formQ" method="post">
-        <div class="form-group-q">
-            <label for="question">Questions</label>
-            <textarea name="question" id="question" cols="30" rows="10"></textarea>
+    <form class="border" method="post">
+        <div class="form-group-q row">
+            <div class="col-sm">
+                <label for="question">Questions</label>
+            </div>
+            <div class="col-md">
+                <textarea class="bg-input" name="question" id="question"></textarea>
+            </div>
         </div>
-        <div class="form-group-q">
-            <label for="nbrPoints">Nbre de points</label>
-            <input type="number" id="nbrPoints" name="nbrPoints">
+        <div class="form-group-q row">
+            <div class="col-sm">
+                <label for="nbrPoints">Nbre de points</label>
+            </div>
+            <div class="col-md">
+                <input class="input-number bg-input" type="number" id="nbrPoints" name="nbrPoints">
+            </div>
         </div>
-        <div class="form-group-q">
-            <label for="typeRep">Type de réponse</label>
-            <select name="typeRep" id="typeRep">
-                <option value="" selected disabled>Donnez le type de réponse</option>
-                <option value="radio">Choix simple</option>
-                <option value="checkbox">Choix multiple</option>
-                <option value="texte">Choix texte</option>
-            </select>
-            <button><img src="/sa_quiz/public/images/icones/ic-ajout.png"></button>
+        <div class="form-group-q row">
+            <div class="col-sm">
+                <label for="typeRep">Type de réponse</label>
+            </div>
+            <div class="col-md input-group">
+                <select class="select-input bg-input" name="typeRep" id="typeRep">
+                    <option value="" selected disabled>Donnez le type de réponse</option>
+                    <option value="radio">Choix simple</option>
+                    <option value="checkbox">Choix multiple</option>
+                    <option value="texte">Choix texte</option>
+                </select>
+                <button class="addInput">+</button>
+            </div>
         </div>
-
+        <div class="response" id="resp">
+            <div class="form-group-q row">
+                <div class="col-sm">
+                    <label for="reponse1">Réponse 1</label>
+                </div>
+                <div class="col-md input-group">
+                    <input class="rep-input bg-input" type="text" id="reponse1" name="reponse1">
+                    <div class="typeChoix">
+                        <input type="checkbox" name="repC" id="repC">
+                        <input type="radio" name="repC" id="repC">
+                        <img src="/sa_quiz/public/images/icones/ic-supprimer">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="enregistrer">
+            <button class="btn btn-md btn-light">Enregistrer</button>
+        </div>
     </form>
 </div>
